fbfdd3fcb37104e5ad09de542e0ed5bc030a8b19
BLADE <abdirahman.ahmed@student.moringaschool.com>
2021-09-14 15:08:07 +0300

    updated logout function

---
+++ netflix-clone-ft_login_passwordchange/movies/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path,include
+from . import views
+from django.contrib.auth import views as auth_views 
+
+urlpatterns = [
+    path('', views.welcome, name='welcome'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    #path('password/', auth_views.PasswordChangeView.as_view()),
+    path('password_change/', views.PasswordsChangeView.as_view(), name='password_change'),
+]
---
+++ netflix-clone-ft_login_passwordchange/movies/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate,login, logout
+from django.contrib.auth.views import PasswordChangeView
+from django.contrib.auth.forms import PasswordChangeForm 
+from django.urls import reverse_lazy
+
+def welcome(request):
+    return render(request, 'welcome.html')
+
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        print(username,password)
+        
+        user = authenticate(request, password=password, username=username)
+        if user is None:
+            context = {"error": "Invalid username or password"}
+            return render(request, "accounts/login.html",context)
+        login(request,user)
+        return redirect('/')
+
+    return render(request, "accounts/login.html")
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+class PasswordsChangeView(PasswordChangeView):
+    form_class = PasswordChangeForm
+    success_url = reverse_lazy('welcome')
+    
